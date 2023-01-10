80d6ef9036086226210bd7ef95b34a80db7cc0b4
Pedra96 <darkraidialga@yahoo.it>
2023-01-10 16:42:30 +0100

    Add project files.

---
+++ La Mia Pizzeria 1/Controllers/PizzaController.cs
@@ -0,0 +1,28 @@
+﻿using La_Mia_Pizzeria_1.Database;
+using La_Mia_Pizzeria_1.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+
+namespace La_Mia_Pizzeria_1.Controllers {
+    public class PizzaController : Controller {
+
+        public IActionResult Index() {
+          using PizzeriaContext db = new PizzeriaContext();
+            List<Pizza> listaDellePizze = db.Pizze.ToList();          
+
+            return View("Index", listaDellePizze);
+        }
+        public IActionResult Details(int id) {
+            using PizzeriaContext db = new PizzeriaContext();
+            List<Pizza> listaDellePizze = db.Pizze.ToList();
+
+            foreach (Pizza post in  listaDellePizze) {
+                if (post.Id == id) {
+                    return View(post);
+                }
+            }
+
+            return NotFound("Il post con l'id cercato non esiste!");
+        }
+    }
+}
---
+++ La Mia Pizzeria 1/Database/PizzeriaContext.cs
@@ -0,0 +1,15 @@
+﻿using La_Mia_Pizzeria_1.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace La_Mia_Pizzeria_1.Database {
+    public class PizzeriaContext:DbContext {
+        public DbSet<Pizza> Pizze { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+
+            optionsBuilder.UseSqlServer("Data Source=localhost;Database=PizzeriaDB;Integrated Security=True;TrustServerCertificate=True");
+
+        }
+
+    }
+}
