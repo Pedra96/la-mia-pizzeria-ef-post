0d3fb756e660dc1dd9b3dc6f26842c5c19b1a44b
Pedra96 <darkraidialga@yahoo.it>
2023-01-12 16:30:41 +0100

    fixato un bug con il validatore img

--- La Mia Pizzeria 1/CustomValidation/ImageUrlValidation.cs
+++ La Mia Pizzeria 1/CustomValidation/ImageUrlValidation.cs
@@ -2,27 +2,32 @@
 using System.Net;
 
 namespace La_Mia_Pizzeria_1.CustomValidation {
-    public class ImageUrlValidation:ValidationAttribute {
+    public class ImageUrlValidation : ValidationAttribute {
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) {
+            string ErrorMessage = "il link inserito non è valido";
             if (value == null) {
                 return new ValidationResult("campo obbligatorio");
             }
 
             string FieldValue = (string)value;
-
             WebRequest webRequest;
             try {
                 webRequest = WebRequest.Create(FieldValue);
-                var x = webRequest.ContentType();
-            }catch(Exception e) {
-                return new ValidationResult("il link inserito non è valido");
+            }
+            catch (Exception e) {
+                return new ValidationResult(ErrorMessage);
             }
             WebResponse webResponse;
             try {
                 webResponse = webRequest.GetResponse();
-                return ValidationResult.Success;
-            }catch(Exception ex) {
-                return new ValidationResult("il link inserito non è valido");
+                string UrlType = webResponse.ContentType;
+                if(UrlType.StartsWith("image/")) {
+                    return ValidationResult.Success;
+                }
+                return new ValidationResult(ErrorMessage);
+            }
+            catch (Exception ex) {
+                return new ValidationResult(ErrorMessage);
             }
 
         }
